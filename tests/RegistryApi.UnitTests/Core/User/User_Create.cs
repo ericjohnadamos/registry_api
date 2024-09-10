f62ce2c87bbe2502ed928d2e6928eabd9d8ded03
Eric John Adamos <ericjohnadamos@gmail.com>
2024-09-10 15:18:51 +0800

    Create additional unit test for creating user

---
+++ tests/RegistryApi.UnitTests/Core/User/User_Create.cs
@@ -0,0 +1,26 @@
+﻿namespace RegistryApi.UnitTests.Core.User;
+
+using RegistryApi.Core.Users;
+using FluentAssertions;
+using Xunit;
+
+public class User_Create
+{
+    [Fact]
+    public void CreateUser_WithValidData_ShouldSucceed()
+    {
+        // Arrange
+        var username = "testuser";
+        var password = "password";
+        var customerId = 1;
+
+        // Act
+        var user = User.CreateNewUser(username, password, customerId);
+
+        // Assert
+        user.Should().NotBeNull();
+        user.Username.Should().Be(username);
+        user.Password.Should().Be(password);
+        user.CustomerId.Should().Be(customerId);
+    }
+}
